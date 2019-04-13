bbf5909f960226aa9dc7afd47ba6526e102182cc
nguyendangthao <nguyendangthao95@gmail.com>
2019-04-13 22:50:14 +0800

    aa

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry } from 'react-native';
+import Home from './src/components/home';
+import { AppContainer } from './config/navigation';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <AppContainer />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
